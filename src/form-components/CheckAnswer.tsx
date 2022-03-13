817eb61897898de304ea540e8ce1ce8cfc852132
dthakar23 <dthakar@udel.edu>
2022-03-13 16:03:16 -0400

    Finished CheckAnswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [givenAnswer, setGivenAnswer] = useState<string>("");
+
+    function compareAnswer(event: ChangeEvent) {
+        setGivenAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="formAnswer">
+                <Form.Label>Check your answer:</Form.Label>
+                <Form.Control value={givenAnswer} onChange={compareAnswer} />
+            </Form.Group>
+            <option>Your answer is: </option>
+            {givenAnswer === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
