7a9fb33d1894ed76ebd240ec0b3b603d799cb924
dthakar23 <dthakar@udel.edu>
2022-03-14 11:39:20 -0400

    fixing linting issue

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
 type ChangeEvent = React.ChangeEvent<
     HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
